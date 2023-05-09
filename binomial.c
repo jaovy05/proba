a4e5da7ef2bead9ec6bea3721ce02a0c66e65cc0
Jaovy05 <joaovitorg.silva050@gmail.com>
2023-05-08 23:32:15 -0300

    coloquei o float com 2 casas decimais e porcentagem, espaço duplo no x removido

--- binomial.c
+++ binomial.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int fat(int f){
     if (f<=1)
         return 1;
@@ -32,11 +33,11 @@ int main(){
     scanf("%d", &n);
     printf("digite o p (taxa de sucesso) em valor relativo: ");
     scanf("%f", &p);
-    printf("digite o x  (quantidade de sucesso): ");
+    printf("digite o x (quantidade de sucesso): ");
     scanf("%d", &x);
     resultado=binominal(n,p,x)*100;
     racumulativo=acumulativo(n,p,x)*100;
-    printf("o binomio e: %f \n", resultado);
-    printf("o binomio acumulativo e: %f \n", racumulativo);
+    printf("o binomio e: %.2f%% \n", resultado);
+    printf("o binomio acumulativo e: %.2f%% \n", racumulativo);
     return 0;
 }
